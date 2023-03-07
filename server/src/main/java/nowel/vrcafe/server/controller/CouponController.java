d9d88518a3245be4d2223d9691b06c5e001b842f
NoWel <119951456+OwenBredenhorst@users.noreply.github.com>
2023-03-07 10:11:44 -0800

    Create CouponController.java

---
+++ server/src/main/java/nowel/vrcafe/server/controller/CouponController.java
@@ -0,0 +1,74 @@
+package nowel.vrcafe.server.controller;
+
+import nowel.vrcafe.server.exeption.UserNotFoundExeption;
+import nowel.vrcafe.server.model.Coupon;
+import nowel.vrcafe.server.model.User;
+import nowel.vrcafe.server.repository.CouponDAO;
+import nowel.vrcafe.server.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * CouponController for all the Mapping
+ *
+ * @author Owen Bredenhorst
+ * @version 1.0
+ * @since 07/03/2023
+ */
+
+@RestController
+public class CouponController {
+
+    @Autowired
+    private CouponDAO repo;
+
+
+
+    /**
+     * Get all users from the database and return them as a list of User objects.
+     *
+     * @return A list of all users in the database.
+     */
+    @GetMapping("/coupons")
+    @CrossOrigin(origins = "http://localhost:3000/")
+    public List<Coupon> getAll() {
+        return repo.getAll();
+    }
+
+
+
+    /**
+     * If the user is not found, then create a new user with the next available id
+     *
+     * @param id The id of the user we want to retrieve.
+     * @return A user object
+     */
+    @GetMapping("/coupon/{id}")
+    @CrossOrigin(origins = "http://localhost:3000/")
+    public Coupon getUserById(
+            @PathVariable int id) {
+
+        Coupon coupon = repo.findByID(id);
+        if (coupon.getId() == 0) {
+            int lastUserIndex = getAll().size() - 1;
+            Coupon last = repo.findByID(lastUserIndex);
+            int uniqueId = last.getId() + 1;
+            coupon.setId(uniqueId);
+            throw new UserNotFoundExeption("id = " + id);
+        }
+        return coupon;
+    }
+
+
+
+
+
+
+}
+
+
